cfb9e18a80bd1a6870a33e96bf7382620d9183ba
Mohamed Saleh Zaied <eng.mohamedszaied@gmail.com>
2018-07-25 17:26:45 +0200

    Update Regular expression

--- runMe.py
+++ runMe.py
@@ -8,31 +8,41 @@ def createDirectoryIfNotExists(fileName):
     if not os.path.exists(os.path.dirname(fileName)):
         os.makedirs(os.path.dirname(fileName))
 
+
 def writeToFile(sourceText, translatedText, target):
     fileName = "output/" + target + ".lproj/Localizable.strings"
     with open(fileName, "a") as myfile:
         createDirectoryIfNotExists(fileName)
-    	contentToWrite = "\"" + sourceText + "\" = \"" + translatedText + "\";\n"
+        print(sourceText)
+        print(translatedText)
+        contentToWrite = sourceText.replace('"', '') + " = \"" + translatedText + "\";\n"
         myfile.write(contentToWrite)
+
 
 def translateSourceText(sourceText, target):
     url = translateAPI + "&q=" + sourceText + "&source=en&target=" + target
+    print(url)
     r = requests.get(url)
-    jsonObject = jsonObject = r.json()
-    return jsonObject['data']['translations'][0]['translatedText'].encode('utf-8')
+    jsonObject = r.json()
+    print(jsonObject)
+    return jsonObject['data']['translations'][0]['translatedText'].encode('utf-8').decode().replace("&quot;", "")
+
 
 def translateLineInFile(line, target, outputTarget):
-    matchSource = re.search(r'\"(.*)\"(.*)\"(.*)\"', line)
+    #matchSource = re.search(r'\"(.*)\"(.*)\"(.*)\"', line)
+    matchSource = re.search(r'(.*)(=)(.*)\"', line)
     if matchSource:
         stringName = matchSource.group(1)
         sourceText = matchSource.group(3)
         translation = translateSourceText(sourceText, target)
         writeToFile(stringName, translation, outputTarget)
 
+
 def clearContentsOfFile(target):
     fileName = "output/" + target + ".lproj/Localizable.strings"
     createDirectoryIfNotExists(fileName)
     open(fileName, 'w').close()
+
 
 def translateFile(translateName, target, outputTarget):
 
@@ -51,12 +61,10 @@ with open('Google_IOS_Country_Codes.txt', 'r') as targetsFile:
         if "#" in targetLine:
             continue
 
-    	targetArray = targetLine.split()
+        targetArray = targetLine.split()
 
-    	translateName = targetArray[0]
-    	translateTarget = targetArray[1]
-    	outputTarget = targetArray[2]
+        translateName = targetArray[0]
+        translateTarget = targetArray[1]
+        outputTarget = targetArray[2]
 
         translateFile(translateName, translateTarget, outputTarget)
-
-
